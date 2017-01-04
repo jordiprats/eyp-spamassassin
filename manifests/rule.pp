55de1c0725002bc999f5ccbb06bdb43a205889a2
Jordi Prats <jprats@systemadmin.es>
2017-01-04 14:37:13 +0100

    bugfix

--- manifests/rule.pp
+++ manifests/rule.pp
@@ -6,7 +6,7 @@ define spamassassin::rule (
                             $description = undef,
                             $order       = '42',
                           ) {
-  concat::fragment{ 'localcf header':
+  concat::fragment{ "localcf ${rulename}":
     target  => '/etc/mail/spamassassin/local.cf',
     order   => '00',
     content => template("${module_name}/rule.erb"),
