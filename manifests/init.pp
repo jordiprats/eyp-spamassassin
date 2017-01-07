1846748541e03500485edf7fb5b2669935586c50
Jordi Prats <jprats@systemadmin.es>
2017-01-07 14:00:31 +0100

    més variables

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,18 +1,22 @@
 class spamassassin(
-                            $manage_package        = true,
-                            $package_ensure        = 'installed',
-                            $manage_service        = true,
-                            $manage_docker_service = true,
-                            $service_ensure        = 'running',
-                            $service_enable        = true,
-                            $spamd_max_children    = '5',
-                            $required_hits         = '5',
-                            $report_safe           = false,
-                            $rewrite_header        = 'Subject [SPAM]',
-                            $use_pyzor             = false,
-                            $skip_rbl_checks       = false,
-                            $rbl_timeout           = '5',
-                          ) inherits spamassassin::params{
+                    $manage_package                  = true,
+                    $package_ensure                  = 'installed',
+                    $manage_service                  = true,
+                    $manage_docker_service           = true,
+                    $service_ensure                  = 'running',
+                    $service_enable                  = true,
+                    $spamd_max_children              = '5',
+                    $required_hits                   = '5',
+                    $report_safe                     = false,
+                    $rewrite_header                  = 'Subject [SPAM]',
+                    $use_pyzor                       = false,
+                    $skip_rbl_checks                 = false,
+                    $rbl_timeout                     = '5',
+                    $use_bayes                       = true,
+                    $use_bayes_rules                 = true,
+                    $bayes_auto_learn                = true,
+                    $bayes_auto_learn_threshold_spam = '10.0',
+                  ) inherits spamassassin::params{
 
   validate_re($package_ensure, [ '^present$', '^installed$', '^absent$', '^purged$', '^held$', '^latest$' ], 'Not a supported package_ensure: present/absent/purged/held/latest')
 
