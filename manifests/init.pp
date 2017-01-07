264a12f0e719964bff9721c827699cd0a3e1f340
Jordi Prats <jprats@systemadmin.es>
2017-01-07 13:57:34 +0100

    variables

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,8 +7,11 @@ class spamassassin(
                             $service_enable        = true,
                             $spamd_max_children    = '5',
                             $required_hits         = '5',
-                            $report_safe           = '0',
+                            $report_safe           = false,
                             $rewrite_header        = 'Subject [SPAM]',
+                            $use_pyzor             = false,
+                            $skip_rbl_checks       = false,
+                            $rbl_timeout           = '5',
                           ) inherits spamassassin::params{
 
   validate_re($package_ensure, [ '^present$', '^installed$', '^absent$', '^purged$', '^held$', '^latest$' ], 'Not a supported package_ensure: present/absent/purged/held/latest')
